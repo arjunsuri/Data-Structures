792ae4cc569a078f2ad83885b7bbe08aac5c52ec
Arjun Suri <arjun.suri.7@gmail.com>
2023-10-14 23:08:48 +0530

    Add files via upload

---
+++ Graphs/Breadth First Search in Graph/BreadthFirstSearch.java
@@ -0,0 +1,68 @@
+
+import java.util.*;
+
+public class BreadthFirstSearch {
+
+    private int v;
+
+    private LinkedList<Integer> adj[];
+
+    BreadthFirstSearch(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        boolean visited[] = new boolean[v];
+
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        // Mark the current node as visited and enqueue it
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            // Get all adjacent vertices of the dequeued
+            // vertex s.
+            // If an adjacent has not been visited,
+            // then mark it visited and enqueue it
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    // Driver code
+    public static void main(String args[]) {
+        BreadthFirstSearch g = new BreadthFirstSearch(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+        System.out.println("Following is Breadth First Traversal (starting from vertex 2)");
+        g.BFS(2);
+    }
+}
